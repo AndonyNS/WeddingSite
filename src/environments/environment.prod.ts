aba9806bc432a1decd715cdb471f5b96e18f7fa7
Andony Nuñez <andony91@gmail.com>
2017-09-22 21:29:12 -0600

    adding environment into prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyBkpbQ-Rr9ZeuKT47btwGlbL7384XDMrLE',
+        authDomain: 'my-wedding-site-474c4.firebaseapp.com',
+        databaseURL: 'https://my-wedding-site-474c4.firebaseio.com',
+        projectId: 'my-wedding-site-474c4',
+        storageBucket: 'my-wedding-site-474c4.appspot.com',
+        messagingSenderId: '811928877231'
+    }
 };
